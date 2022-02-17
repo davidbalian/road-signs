a2435d4381aff2286c38fdccfe3e8fbd4ff33c82
David <davidbalyan9@gmail.com>
2022-02-17 16:31:08 +0200

    added multiple choices

--- src/components/Sign/Sign.js
+++ src/components/Sign/Sign.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-const Sign = ({ name, src }) => {
+const Sign = ({ name, src, r1, r2, r3 }) => {
 	return (
 		<div>
-			<p>{name}</p>
-			<img src={`../signs/${src}`} />
+			<img
+				src={`https://cdn.jsdelivr.net/gh/davidbalian/road-signs/src/signs/${src}`}
+				alt='road sign'
+			/>
+
+			<div className='choices'>
+				<input type='radio' name='caption' value='answer' />
+				<label>{name}</label>
+				<br />
+				<input type='radio' name='caption' value='r1' />
+				<label>{r1}</label>
+				<br />
+				<input type='radio' name='caption' value='r2' />
+				<label>{r2}</label>
+				<br />
+				<input type='radio' name='caption' value='r3' />
+				<label>{r3}</label>
+			</div>
 		</div>
 	);
 };
--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import signs from "../../signs";
 import Sign from "../Sign/Sign";
+import "./Test.css";
 
 const Test = () => {
+	const [i, setI] = useState(0);
+
+	const [name, setName] = useState(signs[i].Caption);
+	const [source, setSource] = useState(signs[i].JPG);
+
+	const generateCaption = () => {
+		let isDifferent = false;
+		let random;
+
+		while (!isDifferent) {
+			random = signs[Math.floor(Math.random() * 661)].Caption;
+
+			if (random !== name) {
+				isDifferent = true;
+			}
+		}
+
+		return random;
+	};
+
 	return (
-		<div>
-			{signs.map((sign) => {
-				return (
-					<Sign
-						name={sign.Caption}
-						src={`https://cdn.jsdelivr.net/gh/davidbalian/road-signs/src/signs/${sign.JPG}`}
-					/>
-				);
-			})}
+		<div className='test-div'>
+			<Sign
+				name={name}
+				src={source}
+				r1={generateCaption()}
+				r2={generateCaption()}
+				r3={generateCaption()}
+			/>
+			<button
+				onClick={() => {
+					setI((i) => i + 1);
+
+					setName(signs[i + 1].Caption);
+					setSource(signs[i + 1].JPG);
+				}}
+			>
+				next
+			</button>
 		</div>
 	);
 };
 
 export default Test;
+
+// <Sign
+// 	name={sign.Caption}
+// 	src={`https://cdn.jsdelivr.net/gh/davidbalian/road-signs/src/signs/${sign.JPG}`}
+// />
